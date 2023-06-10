0900c8c85a789309d03d5ad5d6ec772ce69f86bd
Aris Ripandi <riipandi@gmail.com>
2023-06-10 19:56:46 +0700

    add sample LemonSqueezy tables

--- migrations/20230610124016_create_customers_table.up.sql
+++ migrations/20230610124016_create_customers_table.up.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS customers (
 	id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc() NOT NULL,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255) NOT NULL,
-	password_hash text NOT NULL,
+	billable_id varchar(255) NOT NULL UNIQUE,
+	billable_type varchar(255) NOT NULL UNIQUE,
+	lemon_squeezy_id varchar(255) NOT NULL,
+	trial_ends_at timestamptz,
   created_at timestamptz DEFAULT timezone('utc'::text, now()) NOT NULL,
   updated_at timestamptz DEFAULT timezone('utc'::text, now()) NOT NULL
 );
---
+++ migrations/20230610124050_create_subscriptions_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS subscriptions (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc() NOT NULL,
+	billable_id varchar(255) NOT NULL,
+	billable_type varchar(255) NOT NULL,
+  "type" varchar(255) NOT NULL,
+  lemon_squeezy_id varchar(255) NOT NULL UNIQUE,
+  status varchar(255) NOT NULL,
+  product_id varchar(255) NOT NULL,
+  variant_id varchar(255) NOT NULL,
+  card_brand varchar(255),
+  card_last_four varchar(255),
+  pause_mode varchar(255),
+  pause_resumes_at timestamptz,
+  trial_ends_at timestamptz,
+  renews_at timestamptz,
+  ends_at timestamptz,
+  created_at timestamptz DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at timestamptz DEFAULT timezone('utc'::text, now()) NOT NULL
+);
